743ccfd0d4c30a3b3f78068095b204dce19eba3d
Fabiano Luiz Santos <fab_luiz@msn.com>
2024-04-08 23:33:43 -0300

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import "./Header.module.css"
+import styles from "./Header.module.css"
 
-export function Header(styles) {
+export function Header() {
   return (
     <header className={styles.header}>
       <strong>Ignite Feed</strong>
