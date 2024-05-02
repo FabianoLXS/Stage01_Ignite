566f91e99810cce7ad5a7d4decdbe81fb9edc6ad
Fabiano Luiz Santos <fab_luiz@msn.com>
2024-05-02 16:46:57 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { Header } from './components/Header'
-import {Post} from './Post'
-import './global.css'
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+import styles from "./App.module.css"
+
+import "./global.css"
 
 function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Fabiano Santos"
-        content="blablablalal"
-      />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post author="Fabiano Santos" content="blablablalal" />
+        </main>
+      </div>
     </div>
   )
 }
