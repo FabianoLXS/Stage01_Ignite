e7e7315aeb8da3015679ba642e1ddc281210b0c4
Fabiano Luiz Santos <fab_luiz@msn.com>
2024-05-08 18:35:06 -0300

    commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import styles from "./Comment.module.css"
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/fabianolxs.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>FabianoLXS</strong>
+              <time
+                title="05 de Maio às 19h00"
+                dateTime="2024-05-05 19:00:05"
+              >Cerca de 1h atrás</time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+          <p> Muito bom Devon, parabéns! 👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
