4b1b55a8049c17d0938dd155a57d11016646105b
Fabiano Luiz Santos <fab_luiz@msn.com>
2024-05-23 22:11:36 -0300

    commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import {PencilLine} from 'phosphor-react'
 import styles from "./Sidebar.module.css"
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/fabianolxs.png" alt="" />
+       <Avatar src="https://github.com/fabianolxs.png" />
         <strong>Fabiano Santos</strong>
         <span>Web Developer</span>
       </div>
